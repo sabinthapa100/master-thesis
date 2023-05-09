b83de2b7594c89ae114e6d4f9cbaa0cefdba39e7
pyros-cyber <simonepirota@gmail.com>
2023-05-09 15:02:37 +0200

    change `g_u_rising_exp` def

--- sample/lightpulse.py
+++ sample/lightpulse.py
@@ -98,11 +98,8 @@ Rising exponential pulse shape is optimal to excite the atom
 
 
 def rising_exp(_t, _t0=0., _ap_coupling=1., _loss=1.):
-    if _t > _t0:
-        return 0
-    else:
-        return np.sqrt(_ap_coupling + _loss) * np.exp(
-            (_ap_coupling + _loss) * 0.5 * (_t - _t0))
+    return np.sqrt(_ap_coupling + _loss) * np.exp(
+        (_ap_coupling + _loss) * 0.5 * (_t - _t0))
 
 
 def g_u_rising_exp(_t, _args):
@@ -110,12 +107,12 @@ def g_u_rising_exp(_t, _args):
     _LOSS = _args['loss']
     _T0 = _args['t0']
     _EPS = _args['eps']
-    # if _t > _T0:
-    #     return 0
-    # else:
-    return np.conj(
-        rising_exp(_t, _T0, _AP_COUPLING,
-                   _LOSS)) / np.sqrt(1 - np.exp(_AP_COUPLING + _LOSS) * _EPS)
+    if _t > _T0:
+        return 0
+    else:
+        return np.conj(
+            rising_exp(_t, _t0=_T0, _ap_coupling=_AP_COUPLING, _loss=_LOSS)
+        ) / np.sqrt(1 - np.exp(_AP_COUPLING + _LOSS) * _EPS)
 
 
 ######################
